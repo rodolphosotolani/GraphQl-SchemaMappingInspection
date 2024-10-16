26f7fc02fb8d76eb02050bb7c77e33f61f998abe
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-15 21:09:53 -0400

    feat: adding SchemaMapping in EventController to find sessions from the Event.

--- src/main/java/br/com/rts/estudos/graphql/schemamappinginspection/event/EventController.java
+++ src/main/java/br/com/rts/estudos/graphql/schemamappinginspection/event/EventController.java
@@ -1,12 +1,16 @@
 package br.com.rts.estudos.graphql.schemamappinginspection.event;
 
+import br.com.rts.estudos.graphql.schemamappinginspection.session.Session;
 import br.com.rts.estudos.graphql.schemamappinginspection.utils.DataLoaderUtil;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Log4j2
 @Controller
 public class EventController {
 
@@ -19,7 +23,13 @@ public class EventController {
     }
 
     @QueryMapping
-    public List<Event> events(){
+    public List<Event> events() {
         return events;
+    }
+
+    @SchemaMapping
+    public List<Session> sessions(Event event) {
+        log.info("Generating Sessions from {} ", event.toString());
+        return DataLoaderUtil.getSessions();
     }
 }
