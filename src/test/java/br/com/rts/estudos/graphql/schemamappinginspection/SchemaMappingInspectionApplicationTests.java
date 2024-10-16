8cad293f6839cd9ef44f71938d896acaae81c1da
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-15 20:28:26 -0400

    feat: Create project.

---
+++ src/test/java/br/com/rts/estudos/graphql/schemamappinginspection/SchemaMappingInspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.schemamappinginspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchemaMappingInspectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
